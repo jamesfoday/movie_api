9961077d385fd5dd436b5f5eaa61788a49c880ad
jamesfoday <jamesfodayofficial@gmail.com>
2025-03-19 05:17:22 +0100

    Implemented authentication and authorization
    Installed necessary packages: passport, passport-local, passport-jwt, jsonwebtoken for authentication strategies.

---
+++ passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const Models = require('./models.js'); 
+const Users = Models.User;
+
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+// LocalStrategy
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'Username',
+    passwordField: 'Password'
+  },
+  async (username, password, callback) => {
+    await Users.findOne({ Username: username })
+      .then((user) => {
+        if (!user || user.Password !== password) {
+          return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+        return callback(null, user);
+      })
+      .catch((error) => {
+        if (error) return callback(error);
+      });
+  }
+));
+
+// JWTStrategy - For token-based authentication
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: '458jkele;a785als' 
+}, async (jwtPayload, callback) => {
+  try {
+    const user = await Users.findById(jwtPayload._id);
+    if (!user) {
+      return callback(null, false, { message: 'User not found' });
+    }
+    return callback(null, user);
+  } catch (error) {
+    return callback(error);
+  }
+}));
