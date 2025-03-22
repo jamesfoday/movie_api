8727e1a4f5d0fda8f8f53d0b7bbaac9e1e75857a
jamesfoday <jamesfodayofficial@gmail.com>
2025-03-22 11:23:42 +0100

    fix bug in this code

--- models.js
+++ models.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
-
+// Movie schema
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
@@ -20,11 +19,13 @@ let movieSchema = mongoose.Schema({
   Featured: { type: Boolean, default: false }
 });
 
+// User schema
 const userSchema = new mongoose.Schema({
   Username: { type: String, required: true, unique: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
-  Birthday: { type: Date, required: true }
+  Birthday: { type: Date, required: true },
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] // Reference to Movie model
 });
 
 // Hash the password before saving it
@@ -40,29 +41,19 @@ userSchema.pre('save', function (next) {
   }
 });
 
-// Adding password validation method
+// Method to validate password
 userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
+// Static method to hash password before saving (in case you need it somewhere else)
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, saltRounds); // Using saltRounds constant here
+};
 
-
-
-
-//  Models
+// Create the Models
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
-
-// hash password before saved in the database.
-userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10); // '10' is the salt rounds (complexity)
-};
-
-// Compare harsh password with the one in database 
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password); 
-};
-
 
 // Export Models for use in other files
 module.exports.Movie = Movie;
