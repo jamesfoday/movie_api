a229a6fa1ef5a48a9dd243a31c415804a4bc4449
jamesfoday <jamesfodayofficial@gmail.com>
2025-03-25 21:28:20 +0100

    updated

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const { check, validationResult } = require('express-validator');
 const app = express();
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Listening on Port ${port}`);
+console.log(`Listening on Port ${port}`);
 });
 
 mongoose.connect(CONNECTION_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+useNewUrlParser: true, 
+useUnifiedTopology: true 
 })
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('Error connecting to MongoDB: ', err));
+// .then(() => console.log('Connected to MongoDB Atlas'))
+// .catch((err) => console.error('Error connecting to MongoDB: ', err));
 
 
 
