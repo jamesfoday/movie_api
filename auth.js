09027f188e8d2bf0f8b20801552647b221b5ee26
jamesfoday <jamesfodayofficial@gmail.com>
2025-03-25 11:30:22 +0100

    Auth update

--- auth.js
+++ auth.js
@@ -1,17 +1,21 @@
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-require('./passport'); //  passport Import
+const jwtSecret = 'newSecret17125jesusislove&me'; 
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport'); 
 
 let generateJWTToken = (user) => {
-  return jwt.sign(user, 'newSecret1234!@#$%^', {  
-    subject: user.Username,
-    expiresIn: '7d',
-    algorithm: 'HS256'
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, 
+    expiresIn: '7d', 
+    algorithm: 'HS256' 
   });
-};
+}
 
+
+/* POST login. */
 module.exports = (router) => {
-  
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
@@ -29,4 +33,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-};
+}
