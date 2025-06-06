46979479023e1ae9303a64146d834270b5f819d8
jamesfoday <jamesfodayofficial@gmail.com>
2025-06-07 01:54:40 +0200

    cors request added for angular client side

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ let allowedOrigins = [
   'http://localhost:1234',
   'http://localhost:8080',
   'https://myflix1712.netlify.app',
-  'https://myflix1721.netlify.app'
+  'https://myflix1721.netlify.app',
+  'http://localhost:4200'
 ];
 
 app.use(cors({
